e8d5af61f44b664d898cbc6bfd216b6134358eaa
Utkarsh Pandey <utk2665@gmail.com>
2024-04-17 16:49:02 +0530

    Pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+// eslint-disable-next-line no-unused-vars
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <div className='text-red-500'>
-      App
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
