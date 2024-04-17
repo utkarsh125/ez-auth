464238b09d8f44d9b202f42dda21854728c5f00a
Utkarsh Pandey <utk2665@gmail.com>
2024-04-17 23:19:58 +0530

    Password hashed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,18 @@
 import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
 
 export const signup = async(req, res) => {
     // console.log(req.body);
     const { username, email, password } = req.body;
-    const newUser = new User( { username, email, password} );
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    //OR
+    // const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = new User( { username, email, password: hashedPassword } );
     try{
         await newUser.save();
         res.status(201).json({message: "User created successfully."});
     }catch(error){
         // res.status(500).json({message: "User already exists"});
-        res.status(500).json({message: "User already exists"});
+        res.status(500).json(error.message);
     }
 }
