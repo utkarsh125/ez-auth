42a226580616ba3de68700c3c701cf03d27afbaa
Utkarsh Pandey <utk2665@gmail.com>
2024-04-20 16:59:44 +0530

    Signup Functionalities

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async(req, res) => {
+export const signup = async(req, res, next) => {
     // console.log(req.body);
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -13,6 +14,7 @@ export const signup = async(req, res) => {
         res.status(201).json({message: "User created successfully."});
     }catch(error){
         // res.status(500).json({message: "User already exists"});
-        res.status(500).json(error.message);
+        // res.status(500).json(error.message);
+        next(errorHandler(300, "Something went wrong."));
     }
 }
