942efbaebf1281c482dfea68bd7a9776333887ff
Utkarsh Pandey <utk2665@gmail.com>
2024-05-21 22:35:42 +0530

    Saturation reached.

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,15 +1,36 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from '../redux/user/userSlice';
 
 import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
 
 export default function OAuth() {
+
+  const dispatch = useDispatch();
 
   const handleGoogleClick = async() =>{
     try{
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
-      console.log(result);
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        })
+      })
+      // console.log(result);
+      const data = await res.json();
+      dispatch(signInSuccess(data));
     }catch(error){
       console.log("Could not login with Google", error);
     }
